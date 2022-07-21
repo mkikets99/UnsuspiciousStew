a998cdd3229d6355149a3ed0de03e8d578ea0cb9
Mykola Kikets <mykola.kikets@gmail.com>
2022-07-21 18:15:31 +0300

    1.19 Update

--- src/main/java/schauweg/unsuspiciousstew/StewInfo.java
+++ src/main/java/schauweg/unsuspiciousstew/StewInfo.java
@@ -8,8 +8,7 @@ import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
-import net.minecraft.util.ChatUtil;
+//import net.minecraft.util.ChatUtil;
 import net.minecraft.util.Formatting;
 
 import java.util.List;
@@ -18,10 +17,23 @@ public class StewInfo {
 
     static MinecraftClient minecraft = MinecraftClient.getInstance();
 
+    private static String ticksToSeconds(int duration){
+        duration /= 20;
+        int seconds = 0;
+        int minutes = 0;
+        int hours = 0;
+        seconds = duration % 60;
+        minutes = (duration / 60) % 60;
+        hours = duration / 60 / 60;
+        String sHours = (hours!=0)? hours+":":"";
+        String sMin = ((minutes<=9&&minutes!=0)? "0"+minutes:""+minutes)+":";
+        String sSec = (seconds<=9)? "0"+seconds:""+seconds;
+        return sHours + sMin + sSec;
+    }
     public static void onInjectTooltip(Object stackIn, List<Text> list) {
         ItemStack stack = (ItemStack) stackIn;
         if (stack != null && (stack.getItem() == Items.SUSPICIOUS_STEW)){
-            NbtCompound tag = stack.getTag();
+            NbtCompound tag = stack.getNbt();
             if (tag != null) {
                 NbtList effects = tag.getList("Effects", 10);
                 int effectsCount = effects.size();
@@ -30,8 +42,8 @@ public class StewInfo {
                     tag = effects.getCompound(i);
                     int duration = tag.getInt("EffectDuration");
                     StatusEffect effect = StatusEffect.byRawId(tag.getByte("EffectId"));
-                    list.add(new TranslatableText(effect.getTranslationKey())
-                            .append(" "+ChatUtil.ticksToString(duration))
+                    list.add(Text.translatable(effect.getTranslationKey())
+                            .append(" "+ticksToSeconds(duration))
                             .setStyle(Style.EMPTY.withColor(Formatting.GRAY)));
                 }
             }
--- src/main/java/schauweg/unsuspiciousstew/mixin/MixinInGameHud.java
+++ src/main/java/schauweg/unsuspiciousstew/mixin/MixinInGameHud.java
@@ -16,7 +16,7 @@ import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.text.*;
-import net.minecraft.util.ChatUtil;
+//import net.minecraft.util.ChatUtil;
 import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -50,22 +50,35 @@ public abstract class MixinInGameHud {
     private int scaledWidth;
 
     @Shadow
-    public abstract TextRenderer getFontRenderer();
+    public abstract TextRenderer getTextRenderer();
 
     @Shadow
     private int scaledHeight;
 
+    private String ticksToSeconds(int duration){
+        duration /= 20;
+        int seconds = 0;
+        int minutes = 0;
+        int hours = 0;
+        seconds = duration % 60;
+        minutes = (duration / 60) % 60;
+        hours = duration / 60 / 60;
+        String sHours = (hours!=0)? hours+":":"";
+        String sMin = ((minutes<=9&&minutes!=0)? "0"+minutes:""+minutes)+":";
+        String sSec = (seconds<=9)? "0"+seconds:""+seconds;
+        return sHours + sMin + sSec;
+    }
     @Inject(at = @At("HEAD"), method = "renderHeldItemTooltip")
     public void onInjectTooltip(MatrixStack matrixStack, CallbackInfo info) {
 
         this.client.getProfiler().push("selectedItemName");
         if (this.heldItemTooltipFade > 0 && !this.currentStack.isEmpty()) {
-            MutableText mutableText = (new LiteralText("")).append(this.currentStack.getName()).formatted(this.currentStack.getRarity().formatting);
+            MutableText mutableText = (Text.literal("")).append(this.currentStack.getName()).formatted(this.currentStack.getRarity().formatting);
             if (this.currentStack.hasCustomName()) {
                 mutableText.formatted(Formatting.ITALIC);
             }
 
-            int mainItemNameWidth = this.getFontRenderer().getWidth(mutableText);
+            int mainItemNameWidth = this.getTextRenderer().getWidth(mutableText);
             int j = (this.scaledWidth - mainItemNameWidth) / 2;
             int hotbarOffset = this.scaledHeight - 59;
             if (!this.client.interactionManager.hasStatusBars()) {
@@ -84,10 +97,10 @@ public abstract class MixinInGameHud {
                 int var10001 = j - 2;
                 int var10002 = hotbarOffset - 2;
                 int var10003 = j + mainItemNameWidth + 2;
-                this.getFontRenderer().getClass();
+                this.getTextRenderer().getClass();
                 fill(matrixStack, var10001, var10002, var10003, hotbarOffset + 9 + 2, this.client.options.getTextBackgroundColor(0));
                 if (currentStack.getItem() == Items.SUSPICIOUS_STEW){
-                    NbtCompound tag = currentStack.getTag();
+                    NbtCompound tag = currentStack.getNbt();
                     if (tag != null) {
                         NbtList effects = tag.getList("Effects", 10);
                         int effectsCount = effects.size();
@@ -95,10 +108,10 @@ public abstract class MixinInGameHud {
                             tag = effects.getCompound(i);
                             int duration = tag.getInt("EffectDuration");
                             StatusEffect effect = StatusEffect.byRawId(tag.getByte("EffectId"));
-                            String time = ChatUtil.ticksToString(duration);
-                            Text completeText = new TranslatableText(effect.getTranslationKey()).append(" "+time);
-                            j = (this.scaledWidth - getFontRenderer().getWidth(completeText)) / 2;
-                            this.getFontRenderer().drawWithShadow(matrixStack, completeText, (float)j, (float)hotbarOffset-(i*14)-14, 13421772 + (opacity << 24));
+                            String time = (this.ticksToSeconds(duration));
+                            Text completeText = Text.translatable(effect.getTranslationKey()).append(" "+time);
+                            j = (this.scaledWidth - getTextRenderer().getWidth(completeText)) / 2;
+                            this.getTextRenderer().drawWithShadow(matrixStack, completeText, (float)j, (float)hotbarOffset-(i*14)-14, 13421772 + (opacity << 24));
                         }
                     }
                 }
